df4ec8ffd5d9b6c8809d539f1461c63005b72b5c
Supphawit Noowong <saphawitnoowong@gmail.com>
2025-09-26 15:32:35 +0700

    API Design + Go Fiber
    Finish card API Design + Go Fiber in Trello

--- backend/main.go
+++ backend/main.go
@@ -1,52 +1,55 @@
 package main
 
 import (
-	"context"
 	"log"
-	"os"
-	"time"
 
 	"github.com/gofiber/fiber/v2"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
 	app := fiber.New()
 
-	mongoURI := os.Getenv("MONGO_URI")
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(mongoURI))
-	if err != nil {
-		log.Fatal(err)
-	}
-	collection := client.Database("mydb").Collection("user")
+	app.Get("/users", func(c *fiber.Ctx) error {
+		userId := c.Query("id")
+		if userId == "1" {
+			type User struct {
+				Name      string
+				StudentID string
+				Major     string
+			}
+			user := []User{
+				{
+					Name:      "Supphawit Noowong",
+					StudentID: "65122632",
+					Major:     "COEAI"},
+				{
+					Name:      "Jimmy",
+					StudentID: "65133437",
+					Major:     "COEAI",
+				},
+			}
 
-	app.Get("/api/users", func(c *fiber.Ctx) error {
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-
-		cursor, err := collection.Find(ctx, map[string]interface{}{})
-		if err != nil {
-			return c.Status(500).JSON(fiber.Map{
-				"error":   "Failed to query database",
-				"details": err.Error(),
-			})
-		}
-		defer cursor.Close(ctx)
-
-		var results []map[string]interface{}
-		if err := cursor.All(ctx, &results); err != nil {
-			return c.Status(500).JSON(fiber.Map{
-				"error":   "Failed to decode results",
-				"details": err.Error(),
-			})
+			return c.JSON(user)
+		} else {
+			return c.SendString("Don't have UserId : " + userId)
 		}
 
-		return c.JSON(fiber.Map{
-			"success": true,
-			"count":   len(results),
-			"data":    results,
-		})
+	})
+
+	app.Post("/users/information", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World! Users Postman")
+	})
+
+	app.Put("/users/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World! Users Put")
+	})
+
+	app.Delete("/users", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World! Users Delete")
+	})
+
+	app.Patch("/users", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World! Users Patch")
 	})
 
 	log.Println("🚀 Fiber running on :5000")
